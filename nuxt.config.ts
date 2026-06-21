7da0ab2034d1aec6b43f6f6b7f782a0b788a591a
komkem <komkem.contact@gmail.com>
2026-06-21 21:51:48 +0700

    feat: add NUXT_PUBLIC_API_BASE to runtime configuration and reformat nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,35 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
+
+  runtimeConfig: {
+    public: {
+      apiBase: import.meta.env.NUXT_PUBLIC_API_BASE || "http://localhost:8080",
+    },
+  },
+
   app: {
     head: {
       link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Nunito:wght@300..900&family=Kanit:wght@300..700&display=swap' }
-      ]
-    }
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Nunito:wght@300..900&family=Kanit:wght@300..700&display=swap",
+        },
+      ],
+    },
   },
-  vite: {
-    plugins: [
-      tailwindcss()
-    ]
-  }
-})
 
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
